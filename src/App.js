1ab530bc6b04d522ad329dbbd4059bfd13a45565
Jasur <jasur.marcossoft@gmail.com>
2023-11-18 14:21:13 +0500

    site functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchItems() {
+      try {
+        const response = await fetch("./netlify/functions/getItems");
+        if (response.ok) {
+          const data = await response.json();
+          setItems(data);
+        } else {
+          console.error("faild to fetch items.");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+    fetchItems();
+  }, []);
+
   return (
     <div>
-      <h1>Salom Dunyo</h1>
-      <h1>Hello world</h1>
+      <h1>Items</h1>
+      <ul>
+        {items ? items.map((element, index) => <li>{element.name}</li>) : ""}
+      </ul>
     </div>
-  )
+  );
 }
