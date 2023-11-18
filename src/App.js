8fb1f5277a0b18f5a5449de18ab17b8704fbe8d9
Jasur <jasur.marcossoft@gmail.com>
2023-11-18 13:52:53 +0500

    netlify

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1>Hello world</h1>
       <h1>Salom Dunyo</h1>
-      <h3>bu mening birinchi react loyiham githubdagi</h3>
-      <h3>bu mening birinchi react loyiham githubdagi</h3>
-      <h3>bu mening birinchi react loyiham githubdagi</h3>
-      <h3>bu mening birinchi react loyiham githubdagi</h3>
+      <h1>Hello world</h1>
     </div>
   )
 }
